1093c0029fe5ed35765bf2b96d7cecf398e62533
sebagn <sebagn@hotmail.com>
2024-02-23 20:19:24 -0300

    fix any type

--- app/_common/utils/handleDelete.ts
+++ app/_common/utils/handleDelete.ts
@@ -2,9 +2,9 @@
 import { deleteEtapas } from "@services/etapas";
 import { deletePaciente } from "@services/pacientes";
 
-export const handleDelete = async (paciente) => {
+export const handleDelete = async (paciente: any) => {
     const id = paciente._id;
-    const etapas = paciente?.etapas.map((etapa) => etapa._id) as string[];
+    const etapas = paciente?.etapas.map((etapa: any) => etapa._id) as string[];
     try {
         await deleteEtapas(etapas);
         const response = await deletePaciente(id);
