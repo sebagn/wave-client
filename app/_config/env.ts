1ebf881a4ce26fa5b31f9a345025dd5aa0f0e056
sebagn <sebagn@hotmail.com>
2024-02-23 21:20:04 -0300

    fix env

--- app/_config/env.ts
+++ app/_config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
     api: {
-        url: process.env.HOST || 'https://wave-server-production.up.railway.app/'
+        url: process.env.BACKEND_HOST || 'https://wave-server-production.up.railway.app/'
     }
 };
